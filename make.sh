3808273cc88e9551f7a4bbcd052584b00a78b198
FakeTrader <9635792+FakeTrader@users.noreply.github.com>
2025-04-22 14:53:09 +0800

    awesome packs

---
+++ make.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# 保存工作目录
+WORK_DIR=$(pwd)
+
+cd $WORK_DIR/core-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/ai-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/java-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/python-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/web-pack/
+vsce package
+# code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/document-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR/extra-pack/
+vsce package
+code --install-extension *.vsix
+# vsce publish
+
+cd $WORK_DIR
